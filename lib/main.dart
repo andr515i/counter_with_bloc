c9ff03ca37fb1ec35cc451787f1f3521b6f12a89
Andreas <andr515i@zbc.dk>
2024-03-07 23:06:06 +0100

    snackbar now follow bloc pattern

--- lib/main.dart
+++ lib/main.dart
@@ -21,21 +21,6 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // TRY THIS: Try running your application with "flutter run". You'll see
-            // the application has a purple toolbar. Then, without quitting the app,
-            // try changing the seedColor in the colorScheme below to Colors.green
-            // and then invoke "hot reload" (save your changes or press the "hot
-            // reload" button in a Flutter-supported IDE, or press "r" if you used
-            // the command line to start the app).
-            //
-            // Notice that the counter didn't reset back to zero; the application
-            // state is not lost during the reload. To reset the state, use hot
-            // restart instead.
-            //
-            // This works for code too, not just values: Most code changes can be
-            // tested with just a hot reload.
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
@@ -63,16 +48,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   void _showSnackBar(int count) {
-    setState(() {
-      print(count);
-      if (count % 3 == 0) {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            duration: const Duration(milliseconds: 500),
-            content: Text('Modulus hit: $count')));
-      }
-    });
+    print(count);
+    if (count % 3 == 0) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        duration: const Duration(milliseconds: 500),
+        content: Text('Modulus hit: $count'),
+      ));
+    }
   }
 
   @override
@@ -84,16 +67,35 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: BlocBuilder<CounterChangeBloc, CounterChangeState>(
-          builder: ((context, CounterChangeState state) => Center(
+          builder: ((context, state) => Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     const Text(
                       'You have pushed the button this many times:',
                     ),
-                    Text(
-                      counterBloc.state.currentValue.toString(),
-                      style: Theme.of(context).textTheme.headlineMedium,
+                    // Text(
+                    // counterBloc.state.currentValue.toString(),
+                    //   style: Theme.of(context).textTheme.headlineMedium,
+                    // ),
+                    BlocListener<CounterChangeBloc, CounterChangeState>(
+                      listener: (context, state) {
+                        if (state.currentValue % 3 == 0) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              backgroundColor: Colors.purple,
+                              duration: const Duration(milliseconds: 500),
+                              content:
+                                  Text('Modulus hit:  ${state.currentValue}'),
+                            ),
+                          );
+                        }
+                      },
+                      child: BlocBuilder<CounterChangeBloc, CounterChangeState>(
+                        builder: (context, state) {
+                          return Text('${state.currentValue}');
+                        },
+                      ),
                     ),
                   ],
                 ),
@@ -104,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
             FloatingActionButton(
               onPressed: () {
                 counterBloc.add(CounterIncrementEvent());
-                _showSnackBar(counterBloc.state.currentValue);
+                // _showSnackBar(counterBloc.state.currentValue);
               },
               tooltip: 'Increment',
               child: const Icon(Icons.add),
